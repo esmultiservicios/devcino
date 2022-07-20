3f946c18eeba2aa5ebb533ff33ce91363e1939cd
kiredsolutions <105985102+kiredsolutions@users.noreply.github.com>
2022-07-20 16:59:08 -0600

    Cambios reporte de atencion

---
+++ php/atencion_pacientes_nutricion/generarReporteAtencion.php
@@ -0,0 +1,84 @@
+<?php
+session_start();   
+include "../funtions.php";
+
+header("Content-Type: text/html;charset=utf-8");
+
+include_once "../../dompdf/autoload.inc.php";
+require_once '../../pdf/vendor/autoload.php';
+
+use Dompdf\Dompdf;
+		
+//CONEXION A DB
+$mysqli = connect_mysqli();
+
+date_default_timezone_set('America/Tegucigalpa');
+
+$pacientes_id = $_GET['pacientes_id'];
+$noAtencion = 1;
+$anulada = '';
+
+$queryAtencion = "SELECT CONCAT(co.nombre, ' ',co.apellido) AS 'profesional', CONCAT(p.nombre, ' ',p.apellido) AS 'cliente', p.fecha_nacimiento AS 'fecha_nacimiento', p.email AS 'email', p.telefono1 AS 'telefono', at.*, s.nombre AS 'servicio'
+	FROM atenciones_nutricion AS at
+	INNER JOIN pacientes AS p
+	ON at.pacientes_id = p.pacientes_id
+	INNER JOIN colaboradores AS co
+	ON at.colaborador_id = co.colaborador_id
+	INNER JOIN servicios AS s
+	ON at.servicio_id = s.servicio_id		
+	WHERE at.pacientes_id = '$pacientes_id'";	
+$resultAtencion = $mysqli->query($queryAtencion) or die($mysqli->error);
+
+if($resultAtencion->num_rows>0){
+	$consultaAtencion = $resultAtencion->fetch_assoc();
+	
+	//OBTENER LA EDAD DEL USUARIO 
+	/*********************************************************************************/
+	$valores_array = getEdad($consultaAtencion['fecha_nacimiento']);
+	$anos = $valores_array['anos'];
+	$meses = $valores_array['meses'];	  
+	$dias = $valores_array['dias'];	
+
+	if ($anos>1 ){
+	$palabra_anos = "Años";
+	}else{
+	$palabra_anos = "Año";
+	}
+
+	if ($meses>1 ){
+	$palabra_mes = "Meses";
+	}else{
+	$palabra_mes = "Mes";
+	}
+
+	if($dias>1){
+		$palabra_dia = "Días";
+	}else{
+		$palabra_dia = "Día";
+	}		
+	
+	/*********************************************************************************/
+
+	$image_server = SERVERURL."img/fondo_pagina.jpg";
+	ob_start();
+	include(dirname('__FILE__').'/reporteAtencion.php');
+	$html = ob_get_clean();
+
+	// instantiate and use the dompdf class
+	$dompdf = new Dompdf();
+	
+	$dompdf->set_option('isRemoteEnabled', true);
+
+	$dompdf->loadHtml(utf8_decode(utf8_encode($html)));
+	// (Optional) Setup the paper size and orientation
+	$dompdf->setPaper('letter', 'portrait');
+	// Render the HTML as PDF
+	$dompdf->render();
+	file_put_contents(dirname('__FILE__').'/Reportes/atencion_'.$noAtencion.'.pdf', $dompdf->output());
+	
+	// Output the generated PDF to Browser
+	$dompdf->stream('reporte_'.$noAtencion.'.pdf',array('Attachment'=>0));
+	
+	exit;	
+}
+?>
---
+++ php/atencion_pacientes_nutricion/reporteAtencion.php
@@ -0,0 +1,612 @@
+<html>
+<head>
+  <style>
+	@import url('fonts/BrixSansRegular.css');
+	@import url('fonts/Helvetica.css');
+
+    @page { 
+		margin: 20px 3px; 
+		width: 100%;
+	}
+
+	#footer .page:after { 
+		content: counter(page, upper-roman);
+	}
+
+	p, label, span{
+		font-family: 'Helvetica';
+		font-size: 8pt;
+		word-wrap: break-word;
+		margin: 0 !important;	
+	}
+	h2{
+		font-family: 'Helvetica';
+		font-size: 12pt;
+		word-wrap: break-word;
+		margin: 0 !important;	
+	}
+	#reporte_head, #factura_cliente, #factura_detalle{
+		width: 100%;
+		padding-left: 10px;
+		padding-top: 10px;   
+		padding-bottom: 10px;
+	}
+	.reporte_logo{
+		width: 20%;
+	}
+	#logo{
+		width: 40px;
+		height:auto;
+	}
+	.info_empresa{
+		width: 90%;
+		text-align: center;
+		padding-left: 70px;
+		padding-top: 10px;   
+		padding-bottom: 10px;		
+	}
+	.info_factura{
+		width: 20%;
+	}
+	.info_reporte{
+		width: 100%;
+	}
+	.table{
+		width:100%; 
+		margin: 15 !important;		
+	}
+	.table, .th, .td{
+		border: 1px solid #000;
+		border-spacing: 0;
+		clear:both;
+	}
+    #footer .page:after { 
+		content: counter(page, upper-roman);
+	}	
+  </style>
+</head>
+<body>
+	<div id="content" style="page-break-before: auto;">
+		<table id="reporte_head">
+			<tr>
+				<td class="reporte_logo">
+					<div id="logo">
+						<img src="<?php echo SERVERURL; ?>img/logo_factura.jpg" width="220px" height="80px">
+					</div>
+				</td>
+				<td class="info_empresa">
+					<div>
+						<h2>PRIMERA CONSULTA</h2>
+						<h2>EXPEDIENTE CLINICO</h2>	
+						<h2><?php echo 'Profesional: '.$consultaAtencion['profesional']; ?></h2>									
+					</div>
+				</td>
+				<td class="info_reporte">
+
+				</td>
+			</tr>
+		</table>
+		
+		<table class="table">
+			<tr>
+				<td class="td" width="20%">
+					<p>Apellidos, Nombre</p>					
+				</td>
+				<td class="td" width="80%">
+					<p><?php echo $consultaAtencion['cliente']; ?></p>					
+				</td>
+			</tr>
+		</table>
+
+		<table class="table">
+			<tr>
+				<td class="td" width="20%">
+					<p>Fecha de Nacimiento</p>					
+				</td>
+				<td class="td" width="20%">
+					<p><?php echo $consultaAtencion['fecha_nacimiento']; ?></p>					
+				</td>
+				<td class="td" width="20%">
+					<p>
+						Edad
+					</p>					
+				</td>
+				<td class="td" width="20%">
+					<p><?php echo $anos." ".$palabra_anos.", ".$meses." ".$palabra_mes." y ".$dias." ".$palabra_dia; ?></p>					
+				</td>												
+			</tr>
+		</table>
+
+		<table class="table">
+			<tr>
+				<td class="td" width="10%">
+					<p>NCH</p>					
+				</td>
+				<td class="td" width="55%">
+					<p><?php echo $consultaAtencion['atenciones_nutricion_id']; ?></p>					
+				</td>
+				<td class="td" width="10%">
+					<p>Teléfono</p>					
+				</td>
+				<td class="td" width="25%">
+					<p><?php echo $consultaAtencion['telefono']; ?></p>					
+				</td>											
+			</tr>
+			<tr>
+				<td class="td" width="10%">
+					<p>E-mail</p>					
+				</td>
+				<td class="td" width="55%">
+					<p><?php echo $consultaAtencion['email']; ?></p>					
+				</td>
+				<td class="td" width="10%">
+					<p>Fecha</p>					
+				</td>
+				<td class="td" width="25%">
+					<p><?php echo $consultaAtencion['fecha']; ?></p>					
+				</td>											
+			</tr>			
+		</table>
+
+		<table class="table">
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Motivo Consulta</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['motivo_consulta']; ?></p>				
+				</td>
+			</tr>			
+			<tr>
+				<td class="td" width="50%">
+					<p>Fecha Consulta</p>					
+				</td>				
+				<td class="td" width="50%">
+					<p><?php echo $consultaAtencion['fecha']; ?></p>					
+				</td>																					
+			</tr>
+		</table>
+		
+		<table class="table">
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Antecedentes Personales</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['ante_perso']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Antecedentes Familiares</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['ante_fam']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Alergias</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['alergias']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Adicciones</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['adicciones']; ?></p>				
+				</td>
+			</tr>	
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Niveles de Estres</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['niveles_estres']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Niveles de Actividad Física</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['niveles_actividad_fisica']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Intento Perdidad de Peso</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['intento_perdida_peso']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Antecedentes Quirurgicos</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['antecedentes_quirurgicos']; ?></p>				
+				</td>
+			</tr>					
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Observaciones</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['observaciones']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Diagnostico</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['diagnostico']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Indicaciones</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['indicaciones']; ?></p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Candidato a Bariatrica</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php 
+						if($consultaAtencion['candidato_bariatrica'] == 1){
+							echo 'Sí';
+						}else{
+							echo 'No';
+						}					
+					?></p>				
+				</td>
+			</tr>			
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Servicio</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2">
+					<p><?php echo $consultaAtencion['servicio']; ?></p>				
+				</td>
+			</tr>						
+		</table>		
+
+		<!--
+		<table class="table">
+			<tr>
+				<td class="td" width="12.5%">
+					<p>Inicio Obecidad</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['inicio_obesidad']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Habito Alimenticio</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['habito_alimenticio']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Tipo Obecidad</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['tipo_obesidad']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>Intentos Perdida peso</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['intentos_perdida_peso']; ?></p>					
+				</td>																					
+			</tr>
+
+			<tr>
+				<td class="td" width="12.5%">
+					<p>Peso Maximo Alcansado</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['peso_maximo_alcansado']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Sedentarismo</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['sedentarismo']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Ejercicio</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['ejercicio_respuesta']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>Erge</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_erge']; ?></p>					
+				</td>																				
+			</tr>				
+
+			<tr>
+				<td class="td" width="12.5%">
+					<p>HTA</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_hta']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Higado Graso</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_higado_graso']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>SAOS</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_saos']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>Articulares</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_articulares']; ?></p>					
+				</td>																								
+			</tr>
+
+			<tr>
+				<td class="td" width="12.5%">
+					<p>Ovarios Poliquisticos</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_ovarios_poliquisticos']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Varices</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_varices']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>Drogas</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_drogas']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>Ant Fami Diabetes</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_ant_fami_diabetes']; ?></p>					
+				</td>																								
+			</tr>
+			
+			<tr>
+				<td class="td" width="12.5%">
+					<p>Ant Fami Obecidad</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_ant_fami_obesidad']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Ant Fami Gastrico</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_ant_fami_cancer_gastrico']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>Enf Psiquiatricas</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_respuesta_ant_fami_psiquiatricas']; ?></p>					
+				</td>																					
+				<td class="td" width="12.5%">
+					<p>DM</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_ant_dm']; ?></p>					
+				</td>				
+			</tr>
+							
+			<tr>
+				<td class="td" width="12.5%">
+					<p>Alergias</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_alergias']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Alcohol</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_alcohol']; ?></p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p>Tabaquismo</p>					
+				</td>	
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_tabaquismo']; ?></p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p>Dislipidemia</p>					
+				</td>
+				<td class="td" width="12.5%">
+					<p><?php echo $consultaAtencion['respuesta_dislipidemia']; ?></p>					
+				</td>																									
+			</tr>				
+		</table>
+
+		<table class="table">
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+				<p>Resultado de Examenes</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" >
+					<p><?php echo $consultaAtencion['cirugia_abdominal']; ?></p>				
+				</td>
+			</tr>			
+		</table>
+
+		<table class="table">
+			<tr>
+				<td class="td" width="25%"  colspan="8" align="center" valign="middle">
+					<p>Examen Fisico</p>					
+				</td>				
+			</tr>
+			<tr>
+				<td class="td" width="25%">
+					<p>Talla</p>					
+				</td>
+				<td class="td" width="25%" colspan="3">
+					<p><?php echo $consultaAtencion['talla']; ?></p>					
+				</td>
+				<td class="td" width="25%">
+					<p>Peso</p>					
+				</td>
+				<td class="td" width="25%" colspan="3">
+					<p><?php echo $consultaAtencion['peso']." lbs"; ?></p>					
+				</td>										
+			</tr>
+			<tr>
+				<td class="td" width="25%">
+					<p>IMC</p>					
+				</td>
+				<td class="td" width="25%" colspan="3">
+					<p><?php echo $consultaAtencion['imc']; ?></p>					
+				</td>
+				<td class="td" width="25%">
+					<p>Peso Ideal</p>					
+				</td>
+				<td class="td" width="25%" colspan="3">
+					<p><?php echo $consultaAtencion['peso_ideal']." lbs"; ?></p>					
+				</td>										
+			</tr>
+			<tr>
+				<td class="td" width="25%">
+					<p>Exceso de Peso</p>					
+				</td>
+				<td class="td" width="25%" colspan="3">
+					<p><?php echo $consultaAtencion['exceso_peso']." lbs"; ?></p>					
+				</td>									
+			</tr>												
+		</table>
+			
+		<table class="table">
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+				<p>Hallazgos Anormales al Examen Físico (Diagnostico)</p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" >
+				<p><?php echo $consultaAtencion['diagnostico']; ?></p>			
+				</td>
+			</tr>			
+		</table>
+
+		<table class="table">
+			<tr>
+				<td class="td" width="15%" colspan="2" align="center" valign="middle">
+					<p>Examanes de Laboratorio</p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="15%">
+					<p>Estudios de Imágenes Solicitados</p>					
+				</td>
+				<td class="td" width="85%">
+					<p><?php echo $consultaAtencion['estudios_imagenes']; ?></p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="15%">
+					<p>Referencia A</p>					
+				</td>
+				<td class="td" width="85%">
+					<p><?php echo $consultaAtencion['referencia_a']; ?></p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="15%">
+					<p>Recomendaciones Quirúrgicas</p>					
+				</td>
+				<td class="td" width="85%">
+					<p><?php echo $consultaAtencion['recomendaciones']; ?></p>					
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="15%">
+					<p>Presupuesto Estimado</p>					
+				</td>
+				<td class="td" width="85%">
+					<p><?php echo $consultaAtencion['presupuesto']; ?></p>					
+				</td>
+			</tr>														
+		</table>		
+
+		<table class="table">
+			<tr>
+				<td class="td" width="100%" colspan="2" align="center" valign="middle">
+					<p>Observaciones</p>				
+				</td>
+			</tr>
+			<tr>
+				<td class="td" width="100%" colspan="2" >						
+					<p><?php echo $consultaAtencion['observaciones']; ?></p>			
+				</td>
+			</tr>			
+		</table>
+
+		<table class="table">
+			<tr>
+				<td class="td" width="10%">
+					<p>Servicio</p>					
+				</td>	
+				<td class="td" width="90%">
+				<p><?php echo $consultaAtencion['servicio']; ?></p>					
+				</td>	
+			</tr>		
+		</table>	
+		-->	
+	</div>
+</body>
+</html>
